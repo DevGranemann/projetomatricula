ba7623e50eda8ad9cd393453d757f5bcebc89e77
Eduardo Granemann <167165542+DevGranemann@users.noreply.github.com>
2024-12-29 19:32:34 -0300

    Update processa_aluno.php

--- processa_aluno.php
+++ processa_aluno.php
@@ -1,8 +1,25 @@
 <?php 
+
+    include 'conexao.php';
+    
     $nome_aluno = $_POST['nome'];
     $idade = $_POST['idade'];
     $cpf = $_POST['cpf'];
     $escolaridade = $_POST['escolaridade'];
     
     $insere_aluno = "INSERT INTO aluno (nome_aluno, idade, cpf, escolaridade) VALUES ('$nome_aluno', $idade, $cpf, '$escolaridade')";
+
+    if (mysqli_query($conexao, $insere_aluno))
+    {
+        print "";
+    }
+    else
+    {
+        print "Falha na inserção: " . mysqli_error($conexao);
+    }
+
+    mysqli_close($conexao);
+
+
+?>
 ?>
