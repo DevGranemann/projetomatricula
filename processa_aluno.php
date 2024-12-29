0dc0398a1daf2d732429cd64300631f7a934c761
macbook <macbook@MacBook-Air-de-Eduardo.local>
2024-12-29 00:59:24 -0300

    add processa aluno

---
+++ processa_aluno.php
@@ -0,0 +1,8 @@
+<?php 
+    $nome_aluno = $_POST['nome'];
+    $idade = $_POST['idade'];
+    $cpf = $_POST['cpf'];
+    $escolaridade = $_POST['escolaridade'];
+    
+    $insere_luno = "INSERT INTO aluno (nome_aluno, idade, cpf, escolaridade) VALUES ('$nome_aluno', $idade, $cpf, '$escolaridade')";
+?>
