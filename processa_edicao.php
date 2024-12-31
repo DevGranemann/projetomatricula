39744124e1484f98418c2baa92bf60ea328d914d
Eduardo Granemann <167165542+DevGranemann@users.noreply.github.com>
2024-12-30 23:33:13 -0300

    Add "processa_edicao.php" file

---
+++ processa_edicao.php
@@ -0,0 +1,21 @@
+<?php 
+
+    include 'conexao.php';
+
+    $cod_aluno = $_POST['cod_aluno'];
+    $nome_aluno = $_POST['nome_aluno'];
+    $idade = $_POST['idade'];
+    $cpf = $_POST['cpf'];
+    $escolaridade = $_POST['escolaridade']; 
+
+    $edita_aluno = "UPDATE aluno SET nome_aluno = '$nome_aluno',
+                                            idade = $idade,
+                                            cpf = $cpf,
+                                    escolaridade= '$escolaridade'
+                                    WHERE cod_aluno = $cod_aluno";
+    
+    mysqli_query($conexao, $edita_aluno);
+
+    header('location: ver_aluno.php');
+
+?>
