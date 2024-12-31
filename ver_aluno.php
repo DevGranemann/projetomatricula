71c1df79f31e2a3054c2c8dd061ffad4742a740b
Eduardo Granemann <167165542+DevGranemann@users.noreply.github.com>
2024-12-30 23:38:08 -0300

    Update ver_aluno.php
    Add the new button with the functionality of edit the student`s informations

--- ver_aluno.php
+++ ver_aluno.php
@@ -12,6 +12,8 @@
             <th>Idade</th>
             <th>CPF</th>
             <th>Escolaridade</th>
+            <th>Editar</th>
+            <th>Deletar</th>
         </thead>
         <tbody>
             <?php 
@@ -24,8 +26,14 @@
                     print '<tr><td>' . $linha['nome_aluno'] . '</td>';
                     print '<td>' . $linha['idade'] . '</td>';
                     print '<td>' . $linha['cpf'] . '</td>';
-                    print '<td>' . $linha['escolaridade'] . '</td></tr>';
-                }
+                    print '<td>' . $linha['escolaridade'] . '</td>';
+                ?>  
+
+                    <td><a href="edita_aluno.php?editar= <?php print $linha['cod_aluno']; ?>"><input type="submit" value="EDITAR ALUNO"></a></td>
+
+                    <td><a href="deleta_aluno.php?cod_aluno= <?php print $linha['cod_aluno']; ?>"><input type="submit" value="DELETAR ALUNO"></a></td></tr>
+            <?php
+                }        
             ?>
         </tbody>
     </table>
