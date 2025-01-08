e1881b182a147e383ff1c8ec44de3f030b99ab29
macbook <macbook@MacBook-Air-de-Eduardo.local>
2025-01-07 22:52:30 -0300

    Att de alguns arquivos para correção de bugs

--- edita_curso.php
+++ edita_curso.php
@@ -3,49 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-
-    <?php 
-
-        include 'conexao.php';
-
-        $consulta_curso = "SELECT * FROM curso";
-        $consulta = mysqli_query($conexao, $consulta_curso);
-        while ($linha = mysqli_fetch_array($consulta))
-        {
-            if ($linha['cod_curso'] == $_GET['editar']){ ?>
-
-                <h1>Edição de Cursos</h1>
-                <form action="processa_edCurso.php" method="post">                 
-                    <input type="hidden" name="cod_curso" value="<?php print $linha['cod_curso'];?>">
-                        <label for="nome do curso">Nome do Curso</label>
-                        <input type="text" name="nome_curso" id="idnome_curso" value="<?php print $linha['nome_curso']; ?>">
-                        <br><br>
-                        <label for="Descrição">Descrição</label>
-                        <input type="text" name="descricao" id="iddescricao" value="<?php print $linha['descricao']; ?>">
-                        <br><br>
-                        <label for="carga horaria">Carga Horária</label>
-                        <input type="number" name="carga_horaria" id="idcarga_horaria" value="<?php print $linha['carga_horaria']; ?>">
-                        <br><br>
-                        <label for="área">Área</label>
-                        <input type="text" name="area" id="idarea" value="<?php print $linha['area']; ?>">
-                        <br><br>
-                        <input type="submit" value="EDITAR CURSO">
-                </form>
-            <?php 
-            }
-            ?>
-        <?php 
-        }
-        ?>     
-</body>
-</html><!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style_curso.css">
     <title></title>
 </head>
--- ver_curso.php
+++ ver_curso.php
@@ -3,54 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-    <table border="1">
-        <thead>
-
-            <th>Nome do Curso</th>
-            <th>Descrição</th>
-            <th>Carga Horária</th>
-            <th>Área</th>
-            <th>Editar</th>
-            <th>Deletar</th>
-        </thead>
-        <tbody>
-
-            <?php 
-            
-                include 'conexao.php';
-
-                $consulta_curso = "SELECT * FROM curso";
-                $consulta = mysqli_query($conexao, $consulta_curso);
-
-                while ($linha = mysqli_fetch_array($consulta))
-                {
-                    print '<tr><td>' . $linha['nome_curso'] . '</td>';
-                    print '<td>' . $linha['descricao'] . '</td>';
-                    print '<td>' . $linha['carga_horaria'] . '</td>';
-                    print '<td>' . $linha['area'] . '</td>';
-                
-
-            ?>
-                <td><a href="edita_curso.php?editar= <?php print $linha['cod_curso']; ?>"><input type="submit" value="EDITAR CURSO"></a></td>
-                
-
-                <td><a href="deleta_curso.php?cod_curso= <?php print $linha['cod_curso']; ?>"><input type="submit" value="DELETAR CURSO"></a></td></tr>
-                <?php 
-                }
-                ?>
-
-        </tbody>
-    </table>
-    
-</body>
-</html><!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <script>
         function confirmarExclusao(nomeCurso)
